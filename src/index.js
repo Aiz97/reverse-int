d968ac6351379b0fcbf25a3997d52a139dda134b
Aizat <s.aizat21@gmail.com>
2022-07-28 17:35:07 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const arr = n.toString().split('').map((num)=>{return Number(num)});
+    const reversedArr = arr.reverse().join('');
+    return parseInt(reversedArr);
 }
